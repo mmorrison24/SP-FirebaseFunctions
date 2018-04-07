a7af0c21b984ee69439cd0205cb169ff932f83c9
PrideOfConcept <info@PrideOfConcept.com>
2018-04-07 15:33:32 -0400

    Function: HTTPS retriveCalendar

---
+++ functions/https/getRetriveCalendar.f.js
@@ -0,0 +1,42 @@
+const functions = require('firebase-functions'); // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const admin = require('firebase-admin'); // The Firebase Admin SDK to access the Firebase Realtime Database.
+try {admin.initializeApp(functions.config().firebase);} catch(e) {Function.prototype} // You do that because the admin SDK can only be initialized once.
+//[user includes]
+const express = require('express');
+const app = express();
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+/*
+const authenticate = (req, res, next) => {
+    if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+        res.status(403).send('Unauthorized');
+        return;
+    }
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+    admin.auth().verifyIdToken(idToken).then((decodedIdToken) => {
+        req.user = decodedIdToken;
+        return next();
+    }).catch(() => {
+        res.status(403).send('Unauthorized');
+    });
+};
+
+app.use(authenticate);
+
+
+
+*/
+
+// GET /api/messages?category={category}
+// Get all messages, optionally specifying a category to filter on
+app.get('/', (req, res) => {
+    const category = req.query.category;
+
+    return res.status(200).json({test:true});
+
+});
+
+exports = module.exports = functions.https.onRequest(app);
