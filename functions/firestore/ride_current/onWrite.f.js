9f4b7faf897b53943b1b25936c8ec87ef1b90f14
PrideOfConcept <info@PrideOfConcept.com>
2018-07-26 03:04:38 -0400

    currentRideOnWrite - create to watch creating current_ride

---
+++ functions/firestore/ride_current/onWrite.f.js
@@ -0,0 +1,46 @@
+const functions = require('firebase-functions'); // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const admin = require('firebase-admin'); // The Firebase Admin SDK to access the Firebase Realtime Database.
+try { admin.initializeApp(functions.config().firebase); } catch(e) { Function.prototype } // You do that because the admin SDK can only be initialized once.
+
+// Listens for new data added to rides/{id}
+exports = module.exports = functions.firestore.document('ride_current/{rideID}').onWrite((change, context) => {
+
+    const original = change.before.data(); // Grab the current value of what was written to the firestore Database.
+
+    // get driver info from original
+    const isDelete = !change.after.exists ? true : false
+    const doc = change.after.exists ? change.after.data() : null;
+    const oldDoc = change.before.data()
+
+    const driver = isDelete ? oldDoc.driver : doc.driver
+    const rideId = isDelete ? oldDoc.id : doc.id
+
+    console.log('found isDelete?', isDelete, 'rideId', rideId, 'driver', driver)
+    console.log('data',oldDoc, doc)
+
+    // retrieve camera name from drivers{driver}
+    admin.firestore().collection('drivers')
+        .doc(driver.email)
+        .get()
+        .then( doc => {
+            if ( !doc.exists ) {
+                console.log(driver.email, 'driver not found')
+                return
+            }
+
+            const driverCamera = doc.data().camera
+            console.log('driver found,', doc.data(),' - rideId,',rideId, 'camera:',driverCamera)
+            // set camera{name} to onRide = true
+            admin.firestore().collection('camera')
+                .doc(driverCamera)
+                .set({
+                    currRide: isDelete ? null : rideId,
+                    onRide: isDelete ? false : true
+                }, {merge: true})
+
+            return true;
+        })
+        .catch(err => {console.log('Error getting driver',err)})
+
+});
+
